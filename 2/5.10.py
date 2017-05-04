bd4464d0b5e08a7819786a86a15d29a3bdb54745
Arseny Amerbaev <amerbaev@gmail.com>
2017-05-04 12:05:23 +0300

    ^_^

--- 2/5.10.py
+++ 2/5.10.py
@@ -1,8 +1,8 @@
 numbers = [int(i) for i in input().split(' ')]
 
 if len(numbers) >= 3:
-    sums = [numbers[i - 1] + numbers[i + 1] for i in range(len(numbers) - 1)]
-    sums.append(numbers[0] + numbers[-2])
+    sums = [numbers[i - 1] + numbers[i + 1 - len(numbers)] for i in range(len(numbers))]
+    # sums.append(numbers[0] + numbers[-2])
 else:
     sums = numbers
 
